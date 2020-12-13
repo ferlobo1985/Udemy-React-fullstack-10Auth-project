ced312f48665f1f6b05d0e9fa9dd97bd97b41882
ferlobo1985 <fernando.lobo.prez@gmail.com>
2020-12-13 18:22:11 -0300

    Register a user (no hash)

--- server/server.js
+++ server/server.js
@@ -16,6 +16,19 @@ app.use(bodyParser.json());
 /// MODELS
 const { User } = require('./models/user');
 
+/// ROUTES
+app.post('/api/user',(req,res)=>{
+    const user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save((err,doc)=>{
+        if(err) res.status(400).send(err);
+        res.status(200).send(doc);
+    });
+})
+
+
 
 const port = process.env.PORT || 3001;
 app.listen(port,()=>{
